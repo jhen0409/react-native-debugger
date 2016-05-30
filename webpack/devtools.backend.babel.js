de0039a9c1e10f0a568db1cea8312b3cb7f0392e
Jhen <developer@jhen.me>
2016-05-30 09:56:31 +0800

    Add babel loader include to config of devtools backend

--- webpack/devtools.backend.babel.js
+++ webpack/devtools.backend.babel.js
@@ -3,5 +3,6 @@ import config from 'react-devtools/shells/electron/webpack.backend';
 
 config.context = path.join(__dirname, '../node_modules/react-devtools/shells/electron');
 delete config.module.loaders[0].exclude;
+config.module.loaders[0].include = /..\/node_modules\/react-devtools/;
 
 export default config;
