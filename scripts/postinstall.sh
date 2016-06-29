4c4df5de957de4f5fe8f4feba8ed1bddeac6e0e6
Jhen <developer@jhen.me>
2016-06-29 14:30:27 +0800

    Use self react-devtools branch

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git clone https://github.com/facebook/react-devtools.git node_modules/react-devtools
-cd node_modules/react-devtools && git checkout 627a04ebfea8d6186ebf73c8be42b44f2ecdf346
+git clone https://github.com/jhen0409/react-devtools.git node_modules/react-devtools
+cd node_modules/react-devtools && git checkout rndebugger
 cd ../..
 rm -rf node_modules/react-devtools/.*
 
