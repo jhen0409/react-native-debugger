2ab0930dcdfb6fcb77bf5287e4aef6610dd2fd00
Jhen <developer@jhen.me>
2016-07-04 17:02:50 +0800

    Add Linux / Windows build scripts

--- scripts/package-macos.sh
+++ scripts/package-macos.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm -rf release/
-npm run build
-cd dist && npm i && cd ..
+echo "[v$npm_package_version] Packaging darwin x64..."
+
 electron-packager dist/ \
+  --overwrite \
   --platform darwin \
   --arch x64 \
   --asar \
@@ -11,6 +11,10 @@ electron-packager dist/ \
   --out release \
   --protocol-name "React Native Debugger" \
   --protocol "rndebugger" \
-  --app-version $(node -p -e "require('./package.json').version") \
+  --app-version $npm_package_version \
   --app-copyright "This software is included following project: https://github.com/facebook/react-devtools, https://github.com/zalmoxisus/remotedev-app" \
   --icon electron/logo.icns
+
+rm release/rn-debugger-darwin-x64.dmg
+npm i -g appdmg
+appdmg appdmg.json release/rn-debugger-darwin-x64.dmg
