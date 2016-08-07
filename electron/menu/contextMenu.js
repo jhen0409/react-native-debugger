d98e242de2542e3a6de70f09ed95e0a35e6dd283
Jhen <developer@jhen.me>
2016-08-07 11:36:41 +0800

    Add showInspectElement option for context menu

--- electron/menu/contextMenu.js
+++ electron/menu/contextMenu.js
@@ -2,6 +2,7 @@ import contextMenu from 'electron-context-menu';
 
 export default win =>
   contextMenu({
+    showInspectElement: process.env.NODE_ENV === 'development',
     prepend: () => [{
       label: 'Toggle React DevTools',
       click() {
