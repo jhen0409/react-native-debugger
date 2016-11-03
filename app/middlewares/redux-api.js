1612e123941e9d046a621850f231011c99da0035
Jhen <developer@jhen.me>
2016-11-03 17:44:41 +0800

    Add `redux-api` middleware & Remove createRemoteStore.js

---
+++ app/middlewares/redux-api.js
@@ -0,0 +1,70 @@
+// import { stringify } from 'jsan';
+import { UPDATE_STATE, LIFTED_ACTION } from 'remotedev-app/lib/constants/actionTypes';
+import { DISCONNECTED } from 'remotedev-app/lib/constants/socketActionTypes';
+import { nonReduxDispatch } from 'remotedev-app/lib/utils/monitorActions';
+import { showNotification } from 'remotedev-app/lib/actions';
+import { getActiveInstance } from 'remotedev-app/lib/reducers/instances';
+
+import { SET_DEBUGGER_WORKER } from '../actions/debugger';
+
+let worker;
+let store;
+
+function toWorker({ message, action, state }) {
+  if (!worker) return;
+
+  const instances = store.getState().instances;
+  const instanceId = getActiveInstance(instances);
+  const id = instances.options[instanceId].connectionId;
+  worker.postMessage({
+    method: 'emitReduxMessage',
+    content: {
+      type: message,
+      action,
+      state: nonReduxDispatch(store, message, instanceId, action, state, instances),
+      instanceId,
+      id,
+    },
+  });
+}
+
+// Receive messages from worker
+function messaging(message) {
+  const { data } = message;
+  if (!data || !data.__IS_REDUX_NATIVE_MESSAGE__) return;
+
+  const { content: request } = data;
+  if (request.type === 'ERROR') {
+    store.dispatch(showNotification(request.payload));
+    return;
+  }
+  store.dispatch({
+    type: UPDATE_STATE,
+    request: request.data ? { ...request.data, id: request.id } : request,
+  });
+}
+
+function initWorker(wkr) {
+  wkr.addEventListener('message', messaging);
+  worker = wkr;
+}
+
+function removeWorker() {
+  worker = null;
+}
+
+export default function api(inStore) {
+  store = inStore;
+  return next => action => {
+    if (action.type === SET_DEBUGGER_WORKER) {
+      if (action.worker) {
+        initWorker(action.worker);
+      } else {
+        removeWorker(action.worker);
+        next({ type: DISCONNECTED });
+      }
+    }
+    if (action.type === LIFTED_ACTION) toWorker(action);
+    return next(action);
+  };
+}
--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,12 +1,17 @@
-import { createStore, compose } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { autoRehydrate, persistStore } from 'redux-persist';
+import persist from 'remotedev-app/lib/middlewares/persist';
+import reduxAPI from '../middlewares/redux-api';
 import reducer from '../reducers';
+
+const middlewares = applyMiddleware(reduxAPI, persist());
 
 // If Redux DevTools Extension is installed use it, otherwise use Redux compose
 /* eslint-disable no-underscore-dangle */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 /* eslint-enable no-underscore-dangle */
 const enhancer = composeEnhancers(
+  middlewares,
   autoRehydrate()
 );
 
