0de1409574a00e22b055504040ba11e5b8755cf7
Jhen <developer@jhen.me>
2016-11-04 20:53:12 +0800

    Remove persist middleware of remotedev-app & Add monitor, test to persist whitelist

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,10 +1,9 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { autoRehydrate, persistStore } from 'redux-persist';
-import persist from 'remotedev-app/lib/middlewares/persist';
 import reduxAPI from '../middlewares/redux-api';
 import reducer from '../reducers';
 
-const middlewares = applyMiddleware(reduxAPI, persist());
+const middlewares = applyMiddleware(reduxAPI);
 
 // If Redux DevTools Extension is installed use it, otherwise use Redux compose
 /* eslint-disable no-underscore-dangle */
@@ -17,6 +16,6 @@ const enhancer = composeEnhancers(
 
 export default initialState => {
   const store = createStore(reducer, initialState, enhancer);
-  persistStore(store, { whitelist: ['setting'] });
+  persistStore(store, { whitelist: ['setting', 'monitor', 'test'] });
   return store;
 };
