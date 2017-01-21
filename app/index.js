ab59fe2f86543fb539bdcd285913c2dd5c62119f
Jhen <developer@jhen.me>
2017-01-22 02:18:25 +0800

    Disable zooming for app

--- app/index.js
+++ app/index.js
@@ -1,8 +1,12 @@
+import { webFrame } from 'electron';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './containers/App';
 import configureStore from './store/configureStore';
+
+webFrame.setZoomFactor(1);
+webFrame.setZoomLevelLimits(1, 1);
 
 const store = configureStore();
 
