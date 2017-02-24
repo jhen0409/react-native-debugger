0aacac7ea380bb5a92ed3ea3cb1a88f39a7198f9
Jhen <developer@jhen.me>
2017-02-24 15:58:33 +0800

    Fix file path for patch-react-devtools

--- scripts/patch-react-devtools.sh
+++ scripts/patch-react-devtools.sh
@@ -2,15 +2,12 @@
 
 echo '\n\n## PATCH REACT DEVTOOLS\n'
 
-FILE='./node_modules/react-devtools-core/build/backend.js'
-SOURCE_MAP='./node_modules/react-devtools-core/build/backend.js.map'
+FILE='./node_modules/react-devtools-core/vendor/backend-1.0.6.js'
 
 TARGET='window.requestIdleCallback'
 REPLACE='window.__REQUEST_IDLE_CALLBACK_REPLACED_BY_PATCH__'
 sed -i '' s/$TARGET/$REPLACE/g  $FILE
-sed -i '' s/$TARGET/$REPLACE/g  $SOURCE_MAP
 
 TARGET='window.cancelIdleCallback'
 REPLACE='window.__CANCEL_IDLE_CALLBACK_REPLACED_BY_PATCH__'
 sed -i '' s/$TARGET/$REPLACE/g  $FILE
-sed -i '' s/$TARGET/$REPLACE/g  $SOURCE_MAP
