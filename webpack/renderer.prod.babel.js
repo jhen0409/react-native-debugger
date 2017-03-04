623b0f2eb996686c9bd984c1e714536f7bb1216f
Jhen <developer@jhen.me>
2017-03-04 14:27:09 +0800

    Add babili-webpack-plugin for minify bundle

--- webpack/renderer.prod.babel.js
+++ webpack/renderer.prod.babel.js
@@ -1,4 +1,5 @@
 import webpack from 'webpack';
+import BabiliPlugin from 'babili-webpack-plugin';
 import baseConfig from './base.babel';
 
 export default {
@@ -24,6 +25,9 @@ export default {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
       __REACT_DEVTOOLS_GLOBAL_HOOK__: 'false',
+    }),
+    new BabiliPlugin({
+      comments: false,
     }),
   ],
   resolve: {
