3117b49865b60f19baab3444a8c7afd9d9beb2e3
Jhen <developer@jhen.me>
2017-03-18 22:58:30 +0800

    Make tryADBReverse async

--- app/utils/adb.js
+++ app/utils/adb.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import { execSync } from 'child_process';
+import { exec } from 'child_process';
 
 const androidHome = process.env.ANDROID_HOME || '';
 let adbPath = path.join(androidHome, '/platform-tools/adb');
@@ -8,14 +8,15 @@ if (!fs.existsSync(adbPath)) {
   adbPath = 'adb';
 }
 
-const tryCatch = (fn, arg) => {
-  try {
-    return fn(arg);
-  } catch (err) {} // eslint-disable-line
-};
+const execAsync = (cmd, opts = {}) => new Promise((resolve, reject) =>
+  exec(cmd, opts, (err, result) => {
+    if (err) return reject(err);
+    resolve(result);
+  })
+);
 
-const getDevices = () => {
-  const result = execSync(`${adbPath} devices`, { encoding: 'utf-8' })
+const getDevices = async () => {
+  const result = (await execAsync(`${adbPath} devices`, { encoding: 'utf-8' }))
     .trim()
     .split(/\r?\n/)
     .map(line => {
@@ -29,13 +30,10 @@ const getDevices = () => {
   return result;
 };
 
-const reverse = (device, port) => {
-  execSync(`${adbPath} -s ${device} reverse tcp:${port} tcp:${port}`);
-};
+const reverse = (device, port) =>
+  execAsync(`${adbPath} -s ${device} reverse tcp:${port} tcp:${port}`);
 
-const reverseAllDevices = port => {
-  const devices = getDevices();
-  devices.forEach(device => reverse(device, port));
+export const tryADBReverse = async port => {
+  const devices = await getDevices();
+  return Promise.all(devices.map(device => reverse(device, port)));
 };
-
-export const tryADBReverse = port => tryCatch(reverseAllDevices, port);
