8a006c37031f7a8c51d45265974c564f83dc20d3
Jhen <developer@jhen.me>
2017-04-18 23:37:15 +0800

    Use BrowserWindow.getFocusedWindow for create menu

--- electron/menu/contextMenu.js
+++ electron/menu/contextMenu.js
@@ -1,11 +1,14 @@
+import { BrowserWindow } from 'electron';
 import contextMenu from 'electron-context-menu';
 import { item, n, toggleDevTools } from './util';
 
-export default win =>
+const getWin = () => BrowserWindow.getFocusedWindow();
+
+export default () =>
   contextMenu({
     showInspectElement: process.env.NODE_ENV === 'development',
     prepend: () => [
-      item('Toggle React DevTools', n, () => toggleDevTools(win, 'react')),
-      item('Toggle Redux DevTools', n, () => toggleDevTools(win, 'redux')),
+      item('Toggle React DevTools', n, () => toggleDevTools(getWin(), 'react')),
+      item('Toggle Redux DevTools', n, () => toggleDevTools(getWin(), 'redux')),
     ],
   });
--- electron/menu/darwin.js
+++ electron/menu/darwin.js
@@ -1,53 +1,57 @@
-import { app, shell } from 'electron';
-import {
-  menu, item, separator, n,
-  showAboutDialog, toggleDevTools,
-} from './util';
+import { app, shell, BrowserWindow } from 'electron';
+import { menu, item, separator, n, showAboutDialog, toggleDevTools } from './util';
 
-export default (win, iconPath) =>
-  [
-    menu('React Native Debugger', [
-      item('About', n, () => showAboutDialog(iconPath)),
-      item('Check for Updates...', n, () => win.checkUpdate(win, iconPath, true)),
-      separator,
-      item('Hide', 'Command+H', n, { selector: 'hide:' }),
-      item('Hide Others', 'Command+Shift+H', n, { selector: 'hideOtherApplications:' }),
-      item('Show All', n, n, { selector: 'unhideAllApplications:' }),
-      separator,
-      item('Quit', 'Command+Q', () => app.quit()),
-    ]),
-    menu('Edit', [
-      item('Undo', 'Command+Z', n, { selector: 'undo:' }),
-      item('Redo', 'Shift+Command+Z', n, { selector: 'redo:' }),
-      separator,
-      item('Cut', 'Command+X', n, { selector: 'cut:' }),
-      item('Copy', 'Command+C', n, { selector: 'copy:' }),
-      item('Paste', 'Command+V', n, { selector: 'paste:' }),
-      item('Select All', 'Command+A', n, { selector: 'selectAll:' }),
-    ]),
-    menu('View', [
-      item('Reload', 'Command+R', () => win.webContents.reload()),
-      item('Toggle Full Screen', 'F11', () => win.setFullScreen(!win.isFullScreen())),
-      item('Toggle Developer Tools', 'Alt+Command+I', () => win.toggleDevTools()),
-      item('Toggle React DevTools', 'Alt+Command+J', () => toggleDevTools(win, 'react')),
-      item('Toggle Redux DevTools', 'Alt+Command+K', () => toggleDevTools(win, 'redux')),
-    ]),
-    menu('Window', [
-      item('Minimize', 'Command+M', n, { selector: 'performMiniaturize:' }),
-      item('Close', 'Command+W', n, { selector: 'performClose:' }),
-      separator,
-      item('Bring All to Front', n, n, { selector: 'arrangeInFront:' }),
-      item('Stay in Front', n, ({ checked }) => win.setAlwaysOnTop(checked), {
-        type: 'checkbox',
-        checked: false,
-      }),
-    ]),
-    menu('Help', [
-      item('Documentation', n, () =>
-        shell.openExternal('https://github.com/jhen0409/react-native-debugger#usage')
-      ),
-      item('Issues', n, () =>
-        shell.openExternal('https://github.com/jhen0409/react-native-debugger/issues')
-      ),
-    ]),
-  ];
+const getWin = () => BrowserWindow.getFocusedWindow();
+
+export default iconPath => [
+  menu('React Native Debugger', [
+    item('About', n, () => showAboutDialog(iconPath)),
+    item('Check for Updates...', n, () => {
+      const win = getWin();
+      win.checkUpdate(win, iconPath, true);
+    }),
+    separator,
+    item('Hide', 'Command+H', n, { selector: 'hide:' }),
+    item('Hide Others', 'Command+Shift+H', n, { selector: 'hideOtherApplications:' }),
+    item('Show All', n, n, { selector: 'unhideAllApplications:' }),
+    separator,
+    item('Quit', 'Command+Q', () => app.quit()),
+  ]),
+  menu('Edit', [
+    item('Undo', 'Command+Z', n, { selector: 'undo:' }),
+    item('Redo', 'Shift+Command+Z', n, { selector: 'redo:' }),
+    separator,
+    item('Cut', 'Command+X', n, { selector: 'cut:' }),
+    item('Copy', 'Command+C', n, { selector: 'copy:' }),
+    item('Paste', 'Command+V', n, { selector: 'paste:' }),
+    item('Select All', 'Command+A', n, { selector: 'selectAll:' }),
+  ]),
+  menu('View', [
+    item('Reload', 'Command+R', () => getWin().webContents.reload()),
+    item('Toggle Full Screen', 'F11', () => {
+      const win = getWin();
+      win.setFullScreen(!win.isFullScreen());
+    }),
+    item('Toggle Developer Tools', 'Alt+Command+I', () => getWin().toggleDevTools()),
+    item('Toggle React DevTools', 'Alt+Command+J', () => toggleDevTools(getWin(), 'react')),
+    item('Toggle Redux DevTools', 'Alt+Command+K', () => toggleDevTools(getWin(), 'redux')),
+  ]),
+  menu('Window', [
+    item('Minimize', 'Command+M', n, { selector: 'performMiniaturize:' }),
+    item('Close', 'Command+W', n, { selector: 'performClose:' }),
+    separator,
+    item('Bring All to Front', n, n, { selector: 'arrangeInFront:' }),
+    item('Stay in Front', n, ({ checked }) => getWin().setAlwaysOnTop(checked), {
+      type: 'checkbox',
+      checked: false,
+    }),
+  ]),
+  menu('Help', [
+    item('Documentation', n, () =>
+      shell.openExternal('https://github.com/jhen0409/react-native-debugger#usage'),
+    ),
+    item('Issues', n, () =>
+      shell.openExternal('https://github.com/jhen0409/react-native-debugger/issues'),
+    ),
+  ]),
+];
