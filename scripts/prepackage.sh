5e0d0e347a7133f83cc4f9ac1f184c8c4d964ab4
Jhen <developer@jhen.me>
2017-05-08 20:28:12 +0800

    Clean unnecessary files of dist/node_modules when `run pack`

--- scripts/prepackage.sh
+++ scripts/prepackage.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 npm run build
-cd dist && npm i && cd -
+cd dist
+npm i
+rm -rf node_modules/*/{example,examples,test,tests,*.md,*.markdown,CHANGELOG*,.*,Makefile}
+cd -
 
 ./scripts/patch-react-devtools.sh
