69c60869ec59aad40d92f8172962e6164370a0c3
Jhen <developer@jhen.me>
2017-05-13 22:11:08 +0800

    Fix require modules in test fixture

--- test/e2e/fixture/app.js
+++ test/e2e/fixture/app.js
@@ -1,12 +1,17 @@
 self.window = global;
+
+const requiredModules = {
+  NativeModules: {},
+  Platform: {},
+  setupDevtools: undefined
+};
 window.require = moduleName => {
   // https://github.com/facebook/react-native/blob/5403946f098cc72c3d33ea5cee263fb3dd03891d/packager/src/Resolver/polyfills/require.js#L97
   console.warn(
     'Requiring module \'' + moduleName + '\' by name is only supported for ' +
     'debugging purposes and will BREAK IN PRODUCTION!'
   );
-  if (moduleName !== 'NativeModules') return undefined;
-  return {};
+  return requiredModules[moduleName];
 };
 window.__DEV__ = true;
 
