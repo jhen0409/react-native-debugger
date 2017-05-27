2f8fc1ecbe33e4c8a96981d0429954f928480e0f
Jhen <developer@jhen.me>
2017-05-27 12:25:44 +0800

    Update travis config
    `os` target have been supported `node_js` field

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-git clone https://github.com/creationix/nvm.git /tmp/.nvm
-source /tmp/.nvm/nvm.sh
-nvm install "$NODE_VERSION"
-nvm use --delete-prefix "$NODE_VERSION"
-
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   export DISPLAY=:99.0
   sh -e /etc/init.d/xvfb start
