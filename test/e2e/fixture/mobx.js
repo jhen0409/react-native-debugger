c98539196177c73a99937e88ebdd781012f79431
Jhen <developer@jhen.me>
2017-06-13 10:39:34 +0800

    E2E test: Fix mobx test

--- test/e2e/fixture/mobx.js
+++ test/e2e/fixture/mobx.js
@@ -1,3 +1,4 @@
+/* eslint prefer-arrow-callback: 0 */
 import { observable, action, useStrict } from 'mobx';
 import remotedev from 'mobx-remotedev/lib/dev';
 
@@ -5,12 +6,12 @@ useStrict(true);
 
 export default function run() {
   const store = observable({ value: 0 });
-  store.testPassForMobXStore1 = action(() => {});
+  store.testPassForMobXStore1 = action(function testPassForMobXStore1() {});
 
   remotedev(store, { name: 'MobX store instance 1' }).testPassForMobXStore1();
 
   const store2 = observable({ value: 1 });
-  store2.testPassForMobXStore2 = action(() => {});
+  store2.testPassForMobXStore2 = action(function testPassForMobXStore2() {});
 
   remotedev(store2, { name: 'MobX store instance 2' }).testPassForMobXStore2();
 }
