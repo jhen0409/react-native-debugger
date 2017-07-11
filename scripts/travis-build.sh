720781921afc67a4ee01fe080377242c7c11c7d5
Jhen-Jie Hong <iainst0409@gmail.com>
2017-07-11 17:48:09 +0800

    [CI] Install yarn by brew and apt (#106)
    * [CI] Install yarn by homebrew
    
    * [CI] Install yarn by apt for linux

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -5,13 +5,13 @@ source /tmp/.nvm/nvm.sh
 nvm install "$NODE_VERSION"
 nvm use --delete-prefix "$NODE_VERSION"
 
-# osx_image target haven't built-in yarn
-npm install -g yarn
-
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   export DISPLAY=:99.0
   sh -e /etc/init.d/xvfb start
   sleep 3
+elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  brew update
+  brew install yarn
 fi
 
 node --version
