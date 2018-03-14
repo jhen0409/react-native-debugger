d62c71bad55260e7e526cfd88322d4a07ca3a6a2
Jhen <developer@jhen.me>
2018-03-14 13:46:21 +0800

    E2E test: Update expected logs

--- __e2e__/app.spec.js
+++ __e2e__/app.spec.js
@@ -69,9 +69,6 @@ describe('Application launch', () => {
     await delay(2000);
     const title = await browserWindow.getTitle();
     expect(title).toBe('React Native Debugger - Attempting reconnection (port 8081)');
-
-    // Avoid RND clear logs
-    await app.webContents.executeJavaScript('console.clear = noop => noop');
   });
 
   it('should portfile (for debugger-open usage) always exists in home dir', async () => {
@@ -316,9 +313,16 @@ describe('Application launch', () => {
       logs.forEach(log =>
         console.log(`Message: ${log.message}\nSource: ${log.source}\nLevel: ${log.level}`)
       );
-      expect(logs.length).toEqual(1);
-      const [formDataWarning] = logs;
+      expect(logs.length).toEqual(2);
+      const [notFoundDeltaBundleError, formDataWarning] = logs;
 
+      expect(notFoundDeltaBundleError.source).toBe('network');
+      expect(notFoundDeltaBundleError.level).toBe('SEVERE');
+      expect(
+        notFoundDeltaBundleError.message.indexOf(
+          'app.delta.js - Failed to load resource: net::ERR_FILE_NOT_FOUND'
+        ) > 0
+      ).toBeTruthy();
       expect(formDataWarning.source).toBe('worker');
       expect(formDataWarning.level).toBe('WARNING');
       expect(
