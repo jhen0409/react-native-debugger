a13668b9fa6bdc71d5d76566cb8b870d30f38e94
Jhen <developer@jhen.me>
2018-03-16 17:34:30 +0800

    Update regex pattern

--- app/middlewares/delta/patchFetchPolyfill.js
+++ app/middlewares/delta/patchFetchPolyfill.js
@@ -5,9 +5,8 @@
  * so we could use it to fix the issue.
  */
 const isFetch = '"node_modules/whatwg-fetch/fetch.js"';
-const flag = /if \(self.fetch\) {\n\s+return;\n\s+}\n\s+var support = {/g;
-const replaceStr =
-  'if (self.fetch) {\n      return;\n    }\n    var support = self._fetchSupport = {';
+const flag = /(if \(self.fetch\) {\n\s+return;\n\s+}\n\s+var support )(=)( {)/g;
+const replaceStr = '$1= self._fetchSupport =$3';
 
 export const checkFetchExists = code => code.indexOf(isFetch) > -1;
 export const patchFetchPolyfill = code => code.replace(flag, replaceStr);
