ab18d8f5c091feb6a69148da86fe835dd77b3e6e
Jhen <developer@jhen.me>
2018-06-17 14:51:41 +0800

    Upgrade Expo / RN versions in examples

--- examples/counter-with-redux/src/configureStore.js
+++ examples/counter-with-redux/src/configureStore.js
@@ -5,11 +5,9 @@ import thunk from 'redux-thunk';
 import reducer from './reducers';
 
 const middlewares = [thunk];
-const enhancer = composeWithDevTools(
-  {
-    // Options: https://github.com/jhen0409/react-native-debugger#options
-  },
-)(applyMiddleware(...middlewares));
+const enhancer = composeWithDevTools({
+  // Options: https://github.com/jhen0409/react-native-debugger#options
+})(applyMiddleware(...middlewares));
 
 export default function configureStore(initialState) {
   const store = createStore(reducer, initialState, enhancer);
