7bf2df0f5ceffb1f5e456979b69b56ddee158280
Jhen <developer@jhen.me>
2018-07-24 23:38:11 +0800

    Fix buildTestBundle of E2E tests

--- __e2e__/buildTestBundle.js
+++ __e2e__/buildTestBundle.js
@@ -1,15 +1,21 @@
+import path from 'path';
 import webpack from 'webpack';
 
-export const bundlePath = '__e2e__/fixture/app.bundle.js';
+const outputPath = '__e2e__/fixture';
+const filename = 'app.bundle.js';
+
+export const bundlePath = path.join(outputPath, filename);
 
 // Build a bundle for simulate RNDebugger worker run react-native bundle,
 // it included redux, mobx, remotedev tests
 export default function buildTestBundle() {
   return new Promise(resolve =>
     webpack({
+      mode: 'development',
       entry: './__e2e__/fixture/app',
       output: {
-        filename: `./${bundlePath}`,
+        path: path.resolve(__dirname, '..', outputPath),
+        filename,
       },
       resolve: {
         mainFields: ['main', 'browser'],
