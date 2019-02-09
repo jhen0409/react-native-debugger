ecac16ac88e221e2451eb6f232664a24ba579080
jhen <developer@jhen.me>
2019-02-09 13:57:45 +0800

    Update CI script

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -7,6 +7,9 @@ source /tmp/.nvm/nvm.sh
 nvm install "$NODE_VERSION"
 nvm use --delete-prefix "$NODE_VERSION"
 
+node --version
+npm --version
+
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   export DISPLAY=:99.0
   sh -e /etc/init.d/xvfb start
@@ -18,9 +21,7 @@ elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   brew install applesimutils
 fi
 
-node --version
-npm --version
-
+cd __e2e__/TestApp && yarn && cd ..
 yarn
 cd npm-package && yarn && cd ..
 yarn lint
@@ -29,8 +30,6 @@ yarn build
 yarn test-e2e
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  cd __e2e__/TestApp
-  yarn
   yarn build-test-ios
   yarn test-ios
 fi
