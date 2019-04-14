e2b3a57a980632294f1ca3b30ac4e61ce3ef5b50
jhen <developer@jhen.me>
2019-04-14 11:38:12 +0800

    Update dmg background

--- scripts/package-macos.sh
+++ scripts/package-macos.sh
@@ -2,27 +2,22 @@
 
 echo "[v$npm_package_version] Packaging darwin x64..."
 
-electron-packager dist/ \
-  --overwrite \
-  --platform darwin \
-  --arch x64 \
-  --asar \
-  --no-prune \
-  --out release \
-  --protocol-name "React Native Debugger" \
-  --protocol "rndebugger" \
-  --electron-version $(node -e "console.log(require('electron/package').version)") \
-  --app-version $npm_package_version \
-  --osx-sign='Developer ID Application: Jhen Jie Hong (C6EUM5DVB3)' \
-  --icon electron/logo.icns \
-  --darwin-dark-mode-support
+# electron-packager dist/ \
+#   --overwrite \
+#   --platform darwin \
+#   --arch x64 \
+#   --asar \
+#   --no-prune \
+#   --out release \
+#   --protocol-name "React Native Debugger" \
+#   --protocol "rndebugger" \
+#   --electron-version $(node -e "console.log(require('electron/package').version)") \
+#   --app-version $npm_package_version \
+#   --osx-sign='Developer ID Application: Jhen Jie Hong (C6EUM5DVB3)' \
+#   --icon electron/logo.icns \
+#   --darwin-dark-mode-support
 
-electron-installer-dmg release/React\ Native\ Debugger-darwin-x64/React\ Native\ Debugger.app \
-  React\ Native\ Debugger \
-  --title "React Native Debugger" \
-  --icon electron/logo.icns \
-  --overwrite \
-  --dmgPath release/react-native-debugger_$npm_package_version.dmg
+node scripts/createDMG.js
 
 cd release/React\ Native\ Debugger-darwin-x64
 zip -ryq9 ../rn-debugger-macos-x64.zip React\ Native\ Debugger.app
