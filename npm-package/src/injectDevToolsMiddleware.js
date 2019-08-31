fc4ae99cd04752b02b335f5bb76483ebe4995f78
jhen <developer@jhen.me>
2019-08-31 12:14:18 +0800

    [NPM package] Support latest version of @react-native-community/cli

--- npm-package/src/injectDevToolsMiddleware.js
+++ npm-package/src/injectDevToolsMiddleware.js
@@ -11,7 +11,6 @@ const template = fs.readFileSync(fs.existsSync(tmplPath) ? tmplPath : tmplPathIn
 const name = 'react-native-debugger-patch';
 const startFlag = `/* ${name} start */`;
 const endFlag = `/* ${name} end */`;
-const keyFunc = 'launchChromeDevTools';
 
 const flags = {
   'react-native': {
@@ -19,8 +18,9 @@ const flags = {
       target: 'react-native',
       dir: 'local-cli/server/middleware',
       file: 'getDevToolsMiddleware.js',
-      func: `function ${keyFunc}(port, args = '') {`,
-      replaceFunc: `function ${keyFunc}(port, args = '', skipRNDebugger) {`,
+      keyFunc: 'launchChromeDevTools',
+      func: "function launchChromeDevTools(port, args = '') {",
+      replaceFunc: "function launchChromeDevTools(port, args = '', skipRNDebugger) {",
       funcCall: '(port, args, true)',
       args: "port + '&args=' + args",
     },
@@ -28,28 +28,45 @@ const flags = {
       target: 'react-native',
       dir: 'local-cli/server/middleware',
       file: 'getDevToolsMiddleware.js',
-      func: `function ${keyFunc}(host, args = '') {`,
-      replaceFunc: `function ${keyFunc}(host, args = '', skipRNDebugger) {`,
+      keyFunc: 'launchChromeDevTools',
+      func: "function launchChromeDevTools(host, args = '') {",
+      replaceFunc: "function launchChromeDevTools(host, args = '', skipRNDebugger) {",
       funcCall: '(host, args, true)',
       args: "(host && host.split(':')[1] || '8081') + '&args=' + args",
     },
-    '0.59.0-rc.0': {
-      target: '@react-native-community/cli',
-      dir: 'build/commands/server/middleware',
-      file: 'getDevToolsMiddleware.js',
-      func: `function ${keyFunc}(port, args = '') {`,
-      replaceFunc: `function ${keyFunc}(port, args = '', skipRNDebugger) {`,
-      funcCall: '(port, args, true)',
-      args: "port + '&args=' + args",
-    },
+    '0.59.0-rc.0': [
+      {
+        target: '@react-native-community/cli',
+        dir: 'build/commands/server/middleware',
+        file: 'getDevToolsMiddleware.js',
+        keyFunc: 'launchChromeDevTools',
+        func: "function launchChromeDevTools(port, args = '') {",
+        replaceFunc: "function launchChromeDevTools(port, args = '', skipRNDebugger) {",
+        funcCall: '(port, args, true)',
+        args: "port + '&args=' + args",
+      },
+      {
+        target: '@react-native-community/cli',
+        dir: 'build/commands/server/middleware',
+        file: 'getDevToolsMiddleware.js',
+        keyFunc: 'launchDevTools',
+        func: 'function launchDevTools({\n  port,\n  watchFolders\n}, isDebuggerConnected) {',
+        replaceFunc:
+          'function launchDevTools({port, watchFolders},' +
+          ' isDebuggerConnected, skipRNDebugger) {',
+        funcCall: '({port, watchFolders}, isDebuggerConnected, true)',
+        args: "port + '&watchFolders=' + watchFolders.map(f => `\"${f}\"`).join(',')",
+      },
+    ],
   },
   'react-native-macos': {
     '0.0.0': {
       target: 'react-native-macos',
       dir: 'local-cli/server/middleware',
       file: 'getDevToolsMiddleware.js',
-      func: `function ${keyFunc}(port) {`,
-      replaceFunc: `function ${keyFunc}(port, skipRNDebugger) {`,
+      keyFunc: 'launchChromeDevTools',
+      func: 'function launchChromeDevTools(port) {',
+      replaceFunc: 'function launchChromeDevTools(port, skipRNDebugger) {',
       funcCall: '(port, true)',
       args: 'port',
     },
@@ -59,8 +76,9 @@ const flags = {
     target: 'react-native',
     dir: 'local-cli/server/middleware',
     file: 'getDevToolsMiddleware.js',
-    func: `function ${keyFunc}(port) {`,
-    replaceFunc: `function ${keyFunc}(port, skipRNDebugger) {`,
+    keyFunc: 'launchChromeDevTools',
+    func: 'function launchChromeDevTools(port) {',
+    replaceFunc: 'function launchChromeDevTools(port, skipRNDebugger) {',
     funcCall: '(port, true)',
     args: 'port',
   },
@@ -83,20 +101,12 @@ function getFlag(moduleName, version) {
   return flag;
 }
 
-export const inject = (modulePath, moduleName) => {
-  const info = getModuleInfo(modulePath, moduleName);
-  const {
-    func: funcFlag,
-    replaceFunc: replaceFuncFlag,
-    funcCall,
-    args,
-    target,
-    dir,
-    file,
-  } = getFlag(info.name, info.version);
+const injectCode = (
+  modulePath,
+  { keyFunc, func: funcFlag, replaceFunc: replaceFuncFlag, funcCall, args, target, dir, file }
+) => {
   const filePath = join(modulePath, target, dir, file);
   if (!fs.existsSync(filePath)) return false;
-
   const code = es6Template(template, {
     startFlag,
     replaceFuncFlag,
@@ -113,6 +123,7 @@ export const inject = (modulePath, moduleName) => {
     start = middlewareCode.indexOf(funcFlag);
     end = start + funcFlag.length;
   }
+  if (start === -1) return false;
   fs.writeFileSync(
     filePath,
     middlewareCode.substr(0, start) + code + middlewareCode.substr(end, middlewareCode.length)
@@ -120,20 +131,39 @@ export const inject = (modulePath, moduleName) => {
   return true;
 };
 
-export const revert = (modulePath, moduleName) => {
+export const inject = (modulePath, moduleName) => {
   const info = getModuleInfo(modulePath, moduleName);
-  const { func: funcFlag, target, dir, file } = getFlag(info.name, info.version);
+  const flagList = getFlag(info.name, info.version);
+  if (Array.isArray(flagList)) {
+    flagList.some(flag => injectCode(modulePath, flag));
+  } else {
+    injectCode(modulePath, flagList);
+  }
+  return true;
+};
+
+const revertCode = (modulePath, { func: funcFlag, target, dir, file }) => {
   const filePath = join(modulePath, target, dir, file);
   if (!fs.existsSync(filePath)) return false;
 
   const middlewareCode = fs.readFileSync(filePath, 'utf-8');
   const start = middlewareCode.indexOf(startFlag); // already injected ?
   const end = middlewareCode.indexOf(endFlag) + endFlag.length;
-  if (start !== -1) {
-    fs.writeFileSync(
-      filePath,
-      middlewareCode.substr(0, start) + funcFlag + middlewareCode.substr(end, middlewareCode.length)
-    );
+  if (start === -1) return false;
+  fs.writeFileSync(
+    filePath,
+    middlewareCode.substr(0, start) + funcFlag + middlewareCode.substr(end, middlewareCode.length)
+  );
+  return true;
+};
+
+export const revert = (modulePath, moduleName) => {
+  const info = getModuleInfo(modulePath, moduleName);
+  const flagList = getFlag(info.name, info.version);
+  if (Array.isArray(flagList)) {
+    flagList.some(flag => revertCode(modulePath, flag));
+  } else {
+    revertCode(modulePath, flagList);
   }
   return true;
 };
