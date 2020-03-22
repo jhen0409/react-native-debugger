57bf920605d8fd0fa4caad4a9838a1b200e67feb
jhen <developer@jhen.me>
2020-03-22 08:06:58 +0800

    Update electron/extensions.js

--- electron/extensions.js
+++ electron/extensions.js
@@ -1,25 +1,36 @@
 import path from 'path';
-import { BrowserWindow } from 'electron';
+import { session } from 'electron';
 
 export default async () => {
   if (process.env.NODE_ENV === 'development') {
-    const installer = require('electron-devtools-installer'); // eslint-disable-line
+    // TODO:
+    // const installer = require('electron-devtools-installer'); // eslint-disable-line
 
-    const extensions = ['REACT_DEVELOPER_TOOLS', 'REDUX_DEVTOOLS'];
-    const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-    for (const name of extensions) {
-      try {
-        await installer.default(installer[name], forceDownload);
-      } catch (e) {} // eslint-disable-line
-    }
-    BrowserWindow.addDevToolsExtension(path.resolve('dist/devtools-helper/'));
-    BrowserWindow.addDevToolsExtension(
-      path.join(__dirname, '../node_modules/apollo-client-devtools/shells/webextension/')
+    // const extensions = ['REACT_DEVELOPER_TOOLS', 'REDUX_DEVTOOLS'];
+    // const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+    // for (const name of extensions) {
+    //   try {
+    //     await installer.default(installer[name], forceDownload);
+    //   } catch (e) {} // eslint-disable-line
+    // }
+    await session.defaultSession.loadExtension(
+      path.resolve('dist/devtools-helper/'),
+    );
+    await session.defaultSession.loadExtension(
+      path.join(
+        __dirname,
+        '../node_modules/apollo-client-devtools/shells/webextension/',
+      ),
     );
   } else {
-    BrowserWindow.addDevToolsExtension(path.join(__dirname, 'devtools-helper/'));
-    BrowserWindow.addDevToolsExtension(
-      path.join(__dirname, 'node_modules/apollo-client-devtools/shells/webextension/')
+    await session.defaultSession.loadExtension(
+      path.join(__dirname, 'devtools-helper/'),
+    );
+    await session.defaultSession.loadExtension(
+      path.join(
+        __dirname,
+        'node_modules/apollo-client-devtools/shells/webextension/',
+      ),
     );
   }
 };
