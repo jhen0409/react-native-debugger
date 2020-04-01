14ffa062359ca6f3a8a945358bfa52e71b145ebd
jhen <developer@jhen.me>
2020-04-02 06:58:50 +0800

    Add more message for DevTools v3 incompatible message

--- scripts/patch-modules.js
+++ scripts/patch-modules.js
@@ -5,10 +5,17 @@ console.log('Patch react-devtools-core');
 
 const rdStandalone = path.join(
   __dirname,
-  '../dist/node_modules/react-devtools-core/build/standalone.js'
+  '../dist/node_modules/react-devtools-core/build/standalone.js',
 );
 // Hide source map of react-devtools-core
 // for optimize chrome devtools
+shell.sed(
+  '-i',
+  'DevTools v3 is incompatible with this version of React',
+  'DevTools v3 is incompatible with this version of React' +
+    ', you need manually upgrade React Native Debugger to v0.11. (Please visit the Release page)',
+  rdStandalone,
+);
 shell.sed('-i', '//# sourceMappingURL=standalone.js.map', '', rdStandalone);
 // Avoid logging from react-devtools-core
 // log: connected, disconnected
@@ -18,7 +25,7 @@ shell.sed(
   // eslint-disable-next-line
   /\(_console[0-9]* = console\).(log|warn|error).apply\(_console[0-9]*, \[ "\[React DevTools\]" \].concat\(args\)\)/,
   '',
-  rdStandalone
+  rdStandalone,
 );
 
 console.log('Patch react-dev-utils/launchEditor');
@@ -27,5 +34,5 @@ shell.sed(
   '-i',
   /require\('chalk'\)/g,
   '{red:f=>f,cyan:f=>f,green:f=>f}',
-  path.join(__dirname, '../node_modules/react-dev-utils/launchEditor.js')
+  path.join(__dirname, '../node_modules/react-dev-utils/launchEditor.js'),
 );
