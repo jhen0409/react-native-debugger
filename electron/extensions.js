4bc65e7dee3ae1deb860c99a94e3cba632bafb2b
jhen <developer@jhen.me>
2020-07-05 08:29:07 +0800

    Uncomment load extensions on dev

--- electron/extensions.js
+++ electron/extensions.js
@@ -3,16 +3,15 @@ import { session } from 'electron';
 
 export default async () => {
   if (process.env.NODE_ENV === 'development') {
-    // TODO:
-    // const installer = require('electron-devtools-installer'); // eslint-disable-line
+    const installer = require('electron-devtools-installer'); // eslint-disable-line
 
-    // const extensions = ['REACT_DEVELOPER_TOOLS', 'REDUX_DEVTOOLS'];
-    // const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-    // for (const name of extensions) {
-    //   try {
-    //     await installer.default(installer[name], forceDownload);
-    //   } catch (e) {} // eslint-disable-line
-    // }
+    const extensions = ['REACT_DEVELOPER_TOOLS', 'REDUX_DEVTOOLS'];
+    const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+    for (const name of extensions) {
+      try {
+        await installer.default(installer[name], forceDownload);
+      } catch (e) {} // eslint-disable-line
+    }
     await session.defaultSession.loadExtension(
       path.resolve('dist/devtools-helper/'),
     );
