45dff33a282ee30bd5098fe3119e6dcf67a27649
jhen <developer@jhen.me>
2021-05-04 11:51:27 +0800

    TEMP: Comment setTouchBar

--- app/utils/devMenu.js
+++ app/utils/devMenu.js
@@ -22,15 +22,17 @@ const getBarItems = bar =>
   Object.keys(bar)
     .map(key => bar[key])
     .filter(barItem => !!barItem);
-const setTouchBar = () =>
-  currentWindow.setTouchBar(
-    new remote.TouchBar({
-      items: [
-        ...getBarItems(leftBar),
-        ...(isSliderEnabled ? getBarItems(rightBar) : []),
-      ],
-    })
-  );
+const setTouchBar = () => {
+  // TDOD: Fix crash issue
+  // currentWindow.setTouchBar(
+  //   new remote.TouchBar({
+  //     items: [
+  //       ...getBarItems(leftBar),
+  //       ...(isSliderEnabled ? getBarItems(rightBar) : []),
+  //     ],
+  //   })
+  // );
+}
 
 const invokeDevMenuMethod = ({ name, args }) =>
   worker && worker.postMessage({ method: 'invokeDevMenuMethod', name, args });
@@ -140,8 +142,7 @@ const setDevMenuMethodsForTouchBar = () => {
       click: devMenuMethods.toggleElementInspector,
     });
   }
-  // TDOD: Fix crash issue
-  // setTouchBar();
+  setTouchBar();
 };
 
 // Reset TouchBar when reload the app
