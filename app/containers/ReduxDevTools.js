733004b8a913722c6689c09ec71aee7f2ac8ecdc
jhen <developer@jhen.me>
2023-07-23 09:34:37 +0800

    Fix lint errors

--- app/containers/ReduxDevTools.js
+++ app/containers/ReduxDevTools.js
@@ -1,48 +1,41 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Container, Notification } from '@redux-devtools/ui';
 import { clearNotification } from '@redux-devtools/app/lib/esm/actions';
 import Header from '@redux-devtools/app/lib/esm/components/Header';
 import Actions from '@redux-devtools/app/lib/esm/containers/Actions';
 import Settings from '@redux-devtools/app/lib/esm/components/Settings';
 
-class App extends Component {
-  render() {
-    const { section, theme, notification } = this.props;
-    let body;
-    switch (section) {
-      case 'Settings':
-        body = <Settings />;
-        break;
-      default:
-        body = <Actions />;
-    }
+const App = () => {
+  const section = useSelector(state => state.section);
+  const theme = useSelector(state => state.theme);
+  const notification = useSelector(state => state.notification);
 
-    return (
-      <Container themeData={theme}>
-        <Header section={section} />
-        {body}
-        {notification && (
-          <Notification
-            type={notification.type}
-            onClose={this.props.clearNotification}
-          >
-            {notification.message}
-          </Notification>
-        )}
-      </Container>
-    );
+  const dispatch = useDispatch();
+
+  let body;
+  switch (section) {
+    case 'Settings':
+      body = <Settings />;
+      break;
+    default:
+      body = <Actions />;
   }
-}
 
-const mapStateToProps = (state) => ({
-  section: state.section,
-  theme: state.theme,
-  notification: state.notification,
-});
-
-const actionCreators = {
-  clearNotification,
+  return (
+    <Container themeData={theme}>
+      <Header section={section} />
+      {body}
+      {notification && (
+        <Notification
+          type={notification.type}
+          onClose={() => dispatch(clearNotification())}
+        >
+          {notification.message}
+        </Notification>
+      )}
+    </Container>
+  );
 };
 
-export default connect(mapStateToProps, actionCreators)(App);
+export default App;
--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -12,29 +12,22 @@ const persistConfig = {
   storage: localForage,
 };
 
-const persistedReducer = persistReducer(
-  persistConfig,
-  rootReducer,
-);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middlewares = applyMiddleware(
   debuggerAPI,
   exportStateMiddleware,
-  reduxAPI
+  reduxAPI,
 );
 
 // If Redux DevTools Extension is installed use it, otherwise use Redux compose
 /* eslint-disable no-underscore-dangle */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 /* eslint-enable no-underscore-dangle */
-const enhancer = composeEnhancers(
-  middlewares,
-);
+const enhancer = composeEnhancers(middlewares);
 
 export default (callback) => {
   const store = createStore(persistedReducer, undefined, enhancer);
-  const persistor = persistStore(store, null, () => {
-    callback?.(store);
-  });
+  const persistor = persistStore(store, null, () => callback?.(store));
   return { store, persistor };
 };
